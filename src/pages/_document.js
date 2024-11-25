a6f1446f265e08d9d11337c7dab1387da1de2377
Sean Doughty <sean@render.com>
2024-11-25 18:31:08 -0500

    testing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script src="https://cmp.osano.com/AzZf4RUVBUPZc66vt/5eefab51-58ce-4b25-9547-c1676055a663/osano.js"></script>
       <body>
         <Main />
         <NextScript />
